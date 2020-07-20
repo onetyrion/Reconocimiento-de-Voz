40a3c5958f41417a8c707319c9889f0201129ef3
onetyrion <diego.m.tapia.z@gmail.com>
2020-07-20 14:15:04 -0400

    Fix#1

--- app.js
+++ app.js
@@ -2,20 +2,26 @@
 // Esta funciona mediante eventos como click o onchange
 
 //imports buttons and textArea
-const btnStartRecon = document.getElementById("btnStartRecon");
-const btnStopRecon = document.getElementById("btnStartRecon");
-const TextRecon = document.getElementById("btnStartRecon");
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var grammar = '#JSGF V1.0;'
 
 //prepara la variable recon con la herramienta.
 let recon = new webkitSpeechRecognition();
 
+const btnStartRecon = document.getElementById("btnStartRecon");
+const btnStopRecon = document.getElementById("btnStartRecon");
+const TextRecon = document.getElementById("btnStartRecon");
+
+
 //se configura con:
-    // lang : tipo de lenguaje (es-ES || en-US)
-    // continuous : continua grabando mientras nosotros paramos de hablar
-    // interimResults : resultados instantaneos o cuando se para de hablar
+// lang : tipo de lenguaje (es-ES || en-US)
+// continuous : continua grabando mientras nosotros paramos de hablar
+// interimResults : resultados instantaneos o cuando se para de hablar
 recon.lang = 'es-ES';
 recon.continuous = true;
 recon.interimResults = false;
+console.log(recon);
 
 //Evento que devuelve el texto cuando se para de hablar
 recon.onresult = (event) => {
@@ -25,8 +31,9 @@ recon.onresult = (event) => {
 
 //Evento que empieza y detiene la grabación
 btnStartRecon.addEventListener('click',()=>{
-    recon.start()
+    recon.start();
+    console.log(recon);
 });
 btnStopRecon.addEventListener('click',()=>{
-    recon.abort()
+    recon.abort();
 });
